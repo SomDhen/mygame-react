c0ed23275cd6f53bf1ecd901f0cdc8786b9464e9
Sonam Dhendup <somdhendup@gmail.com>
2024-02-26 21:17:13 +0900

    Build platform selector

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "13249a8cd4084ed59070decca416cb4b",
+    key: "6cd37b85bbd34332a957d849dab998f1",
   },
 });
 //
